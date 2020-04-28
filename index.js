875f6d7413bc27be293d377d799e3300acb8de97
Caden Kreppein <cadenkreppein@gmail.com>
2020-04-27 21:36:43 -0400

    testing out how mentions are represented

--- index.js
+++ index.js
@@ -21,9 +21,35 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
 
+sendErrorMessage = (msg) => {
+  msg.channel.send(`Unrecognized command. Type '!gbp help' for a list of commands.`);
+};
+
 client.on('message', msg => {
-  if (msg.content.startsWith('!gbp ')) {
-    msg.channel.send('Hello there!');
+  if (msg.content.startsWith('!gbp')) {
+    var params = msg.content.split(' ');
+    if (params.length === 0) {
+      sendErrorMessage(msg);
+    }
+    var command = params[0];
+
+    if (command === 'give') {
+      var mentions = msg.mentions;
+      for (var m in mentions) {
+        console.log(m);
+      }
+    }
+
+    if (command === 'help') {
+      msg.channel.send(`Type '!gbp' followed by any of the following commands to use your Good Boy Points.
+
+Good Boy Points Commands:
+balance: Check how many GBP you have. (FREE)
+tendies: Buy some tendies. (50 GBP)
+mussy: Buy some honey mustard. (25 GBP)
+ree: Have this bot elicit a mighty REEEEEEEEEEEEEEEEEEEEEE (150 GBP)`);
+    }
+    
   }
 })
 
